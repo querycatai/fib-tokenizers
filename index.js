c1709466126ad42f241578c2fd70cfeb416dbdb6
xicilion <lion@9465.net>
2023-12-28 08:50:23 +0000

    Add BaichuanTokenizer and XLMRobertaTokenizer models

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const path = require("path");
 
 var tokenizers = require(`./addon/${path.basename(__dirname)}.node`);
 
-tokenizers.config_from_name = require('./lib/config_from_name');
 require('./lib/models')(tokenizers);
 
+const tokenizers_index = {};
+
+for (var key in tokenizers)
+    tokenizers_index[key.toLowerCase()] = tokenizers[key];
+
+tokenizers.config_from_name = require('./lib/config_from_name');
 tokenizers.from_folder = module.exports = function (home, model) {
     const model_home = path.join(home, "models--" + model.replace(/\//g, "--"));
     var model_path;
@@ -29,15 +34,16 @@ tokenizers.from_folder = module.exports = function (home, model) {
 
     const model_config = tokenizers.config_from_name(config, tokenizer_config, file_list, special_tokens_map);
 
-    const vocabs = tokenizers[model_config.tokenizer_class].vocabs.map(vocab => fs.readFileSync(path.join(model_path, vocab)));
-    var tokenizer;
+    const tokenizer_class = tokenizers_index[model_config.tokenizer_class.toLowerCase()];
+    const vocabs = tokenizer_class.vocabs.map(vocab => fs.readFileSync(path.join(model_path, vocab)));
 
+    var tokenizer;
     switch (vocabs.length) {
         case 1:
-            tokenizer = new tokenizers[model_config.tokenizer_class](vocabs[0], model_config);
+            tokenizer = new tokenizer_class(vocabs[0], model_config);
             break;
         case 2:
-            tokenizer = new tokenizers[model_config.tokenizer_class](vocabs[0], vocabs[1], model_config);
+            tokenizer = new tokenizer_class(vocabs[0], vocabs[1], model_config);
             break;
         default:
             throw new Error("Unsupported vocabs");
