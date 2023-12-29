009b3f3219300d6a8057cb477c4575500da2c007
xicilion <lion@9465.net>
2023-12-30 06:15:15 +0800

    Add CamembertTokenizer class

--- test/test_models.js
+++ test/test_models.js
@@ -23,11 +23,13 @@ var home = path.join(process.env.HOME, ".cache/huggingface/hub");
 // MBart50Tokenizer sentencepiece.bpe.model
 // NllbTokenizer sentencepiece.bpe.model
 // CamembertTokenizer sentencepiece.bpe.model
+
+// =================================
+
 // M2M100Tokenizer sentencepiece.bpe.model
 // SeamlessM4TTokenizer sentencepiece.bpe.model
 // SpeechT5Tokenizer spm_char.model
 
-// =================================
 
 const test_class = [
     "LlamaTokenizer",
@@ -42,6 +44,7 @@ const test_class = [
     "BaichuanTokenizer",
     "MBart50Tokenizer",
     "NllbTokenizer",
+    "CamembertTokenizer",
     // =================================
 ];
 
